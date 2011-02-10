5832e734232d2e01939ff429f41c34b7ae4ac1b4
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-02-10 19:51:14 +0100

    more kmeans.

--- benchmarks/bench_kmeans.py
+++ benchmarks/bench_kmeans.py
@@ -2,21 +2,48 @@
 
 import numpy as np
 from datetime import datetime
+from shogun.Distance import EuclidianDistance
+from shogun.Features import RealFeatures
+from shogun.Clustering import KMeans
 from scikits.learn import cluster as skl_cluster
+from pybrain.auxiliary import kmeans as pybrain_kmeans
 
 #
 #       .. Generate dataset ..
 #
 n_samples, n_dim = 500, 500
+k = 9
 X = 100 * np.random.randn(n_samples, n_dim)
+
+
+def bench_shogun():
+#
+#       .. Shogun ..
+#
+    start = datetime.now()
+    feat = RealFeatures(X.T)
+    distance=EuclidianDistance(feat, feat)
+    clf=KMeans(k, distance)
+    clf.train()
+    return datetime.now() - start
+
 
 def bench_skl():
 #
 #       .. scikits.learn ..
 #
     start = datetime.now()
-    clf = skl_cluster.KMeans()
+    clf = skl_cluster.KMeans(k=k)
     clf.fit(X)
+    return datetime.now() - start
+
+
+def bench_pybrain():
+#
+#       .. pybrain ..
+#
+    start = datetime.now()
+    pybrain_kmeans.kmeanspp(X, k)
     return datetime.now() - start
 
 
@@ -24,5 +51,6 @@ if __name__ == '__main__':
     print __doc__
     print 'Using %s points, %s dims' % \
           (n_samples, n_dim)
+    print 'Shogun: ', bench_shogun()
     print 'scikits.learn: ', bench_skl()
-
+    print 'pybrain: ', bench_pybrain()

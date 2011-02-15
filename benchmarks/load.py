9f8e38f4cdf3129765baec0bf85553396d4c7777
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-02-15 16:39:12 +0100

    more

--- benchmarks/load.py
+++ benchmarks/load.py
@@ -5,7 +5,7 @@ def load_data():
     f = open('data/gisette_train.data')
     X = np.fromfile(f, dtype=np.float64, sep=' ')
     f.close()
-    X = X.reshape((13500, -1))
+    X = X.reshape(-1, 5000)
 
     f = open('data/gisette_train.labels')
     y = np.fromfile(f, dtype=np.float64, sep=' ')
